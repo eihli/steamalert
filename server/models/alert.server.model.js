b413ca58839bcb3720c67bff93654f8130293cb2
Eric Ihli <ericihli@gmail.com>
2015-06-16 12:14:16 -0700

    First attempt at recovery

---
+++ server/models/alert.server.model.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var  alertSchema = new Schema({
+  email: String,
+  userId: String,
+  gameName: String
+});
+
+
+var Alert = mongoose.model('Alert', alertSchema);
+
+alertSchema.methods.addAlert = function(email, userId, gameName, cb) {
+  var alert = new Alert({
+    email: email,
+    userId: userId,
+    gameName: gameName
+  });
+  alert.save(function(err) {
+    if (err) {
+      console.log("Error trying to save");
+      cb(err);
+    } else {
+      console.log("Saved new alert to database");
+      cb(null, {result: "success"});
+    }
+  });
+};
+
+module.exports = Alert;
